d23ff22a86ad671f08897d316bf2193c6017ea5c
Jesper Sidhu <jesper.sidhu@gmail.com>
2025-11-30 12:06:45 +1100

    Changed to modular structure

---
+++ lua/keymaps.lua
@@ -0,0 +1,74 @@
+-- [[ Basic Keymaps ]]
+--
+--  See `:help vim.keymap.set()`
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- [[ JESPERS KEYMAPS]]
+-- Basic editor changes
+vim.keymap.set('i', 'jj', '<ESC>') -- jj to escape insert mode
+vim.keymap.set('n', 'o', 'o<ESC>') -- o to add new line but not enter insert mode
+vim.keymap.set('n', 'O', 'O<ESC>') -- O to add new line but not enter insert mode
+
+-- Navigation Settings
+vim.keymap.set('n', '<C-u>', '<C-u>zz') -- Up half screen centers after move
+vim.keymap.set('n', '<C-d>', '<C-d>zz') -- Down half screen centers after move
+
+vim.keymap.set('n', 'n', 'nzz') -- Centre cursor after search
+vim.keymap.set('n', 'N', 'Nzz') -- Centre cursor after search
+vim.keymap.set('n', '#', '#zz') -- Centre cursor after search
+vim.keymap.set('n', '*', '*zz') -- Centre cursor after search
+
+vim.keymap.set('n', 'Y', 'y$') -- Yank from cursor to the end of line
+
+vim.keymap.set('n', '<leader><space>v', ':vsplit') -- Split file vertically
+vim.keymap.set('n', '<leader><space>s', ':split') -- Split file horizontally
+
+vim.keymap.set('n', 'K', 'r<CR>') -- Insert line break
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
